b77596791a85c2d8f6b80d72711881a7ffa6c03e
anastasiia <Anastasiia.Lepova@ivkhk.ee>
2021-10-29 11:07:52 +0300

    asd

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,6 +3,7 @@
 defined('BASEPATH') OR exit('No direc script access alowwed');
 
 class Main extends MY_Controller {
+
     public function __contrast() {
         parent::__contrast();
     }
@@ -37,6 +38,68 @@ class Main extends MY_Controller {
 
         $p_config['full_tag_open'] = "<ul class ='pagination'>";
         $p_config['full_tag_close'] = "</ul>";
-        $p_config[''] = "<>";
+        $p_config['num_tag_open'] = "<li>";
+        $p_config['num_tag_close'] = "</li>";
+        $p_config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+        $p_config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+        $p_config['next_tag_open'] = "<li>";
+        $p_config['next-tagl_close'] = "</li>";
+        $p_config['prev_tag_open'] = "<li>";
+        $p_config['prev_tagl_close'] = "</li>";
+        $p_config['first_tag_open'] = "<li>";
+        $p_config['first_tagl_close'] = "</li>";
+        $p_config['last_tag_open'] = "<li>";
+        $p_config['last_tag_close'] = "</li>";
+
+        $this->pagination->initialize($p_config);
+        $this->data['pagination'] = $this->pagination->create_links();
+
+        $this->load->view('templates/header', $this->data);
+        $this->load->view('main/rating', $this->data);
+        $this->load->view('templates/footer');
+    }
+    
+    public function contact() {
+        
+        $this->data['title'] = "Контакты";
+
+        $this->load->library('session');
+        $this->load->library('form_validation');
+        $this->load->library('email');
+
+        $this->form_validation->set_rules('name','Ваше имя', 'trim|required');
+        $this->form_validation->set_rules('email','Ваш емаил', 'trim|required|valid_email');
+        $this->form_validation->set_rules('subject','Тема', 'trim|required');
+        $this->form_validation->set_rules('message','Ваш отзыв', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE ) {
+
+            $this->load->view('templates/header', $this->data);
+            $this->load-view('main/contact', $this->data);
+            $thos->load->view('templates/footer');
+        }
+        else {
+            $name = $this->input->post('name');
+            $from_email = $this->input->post('email');
+            $subject = $this->input->post('subject');
+            $message = $this->input->post('message');
+
+            $to_email = 'info@wh-db.com';
+
+            $this->email->from($from_email, $name);
+            $this->email->to($to_email);
+            $this->email->suject($subject);
+            $this->email->message($message);
+            if ($this->email->send()) {
+                $this->session->set_flashdata('msg', '<div class="alert-success text-center">Ваше сообщение успешно
+                    отправлено!</div>');
+                redirect('/contact');
+            }
+            else {
+                $this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">Произошла ошибка, повторите
+                    попытку позжею</div>');
+                redirect('/contact');
+            }
+        }
     }
 }
